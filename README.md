cbe8b7fd915feb2cd6ab20975ecba1656b629f8a
Danila Shevluk <d.shevluk@dodopizza.com>
2022-12-03 03:03:56 +0300

    Solution for day 1

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-# Advent of Code {year}
+# advent-of-code-2022
 
-🎄 This is a template for solving [Advent of Code](https://adventofcode.com) puzzles in Swift!
+My solution for [Advent of Code 2022](https://adventofcode.com/2022).
 
-How to start solving puzzles:
-1. Start with 'Day 1', see instructions [here](https://adventofcode.com/2022/day/1).
-2. Create your `PuzzleSolver` struct and add it to the solvers array. You can see [an example for 'Day 1'](Sources/AdventOfCode/Solvers/Day1Solver.swift).
-3. Add your input to the [inputs](inputs) folder.
-4. Get an answer with a CLI command:
-    ```sh
-    swift run solve-aoc --day 1 --part two inputs/day1-example
-    ```
+All solutions are packaged as a Swift CLI executable. You can pass arguments to solve a puzzle for
+a specific part of a specific day. You can find example inputs in the [inputs folder](inputs).
 
-🚀 Feel free to submit your ideas for improvements in issues or PR-s.
+Here is how you can get a solution to a puzzle:
+```sh
+swift run solve-aoc --day 1 --part two inputs/day1-example
+```
--- Sources/AdventOfCode/Solvers/Day1Solver.swift
+++ Sources/AdventOfCode/Solvers/Day1Solver.swift
@@ -11,10 +11,38 @@ struct Day1Solver: PuzzleSolver {
     }
 
     private func solvePart1(_ inputData: String) -> String? {
-        nil
+        var maxCalories = 0
+
+        var currentElfCarry = 0
+        for rawCalories in inputData.split(omittingEmptySubsequences: false, whereSeparator: \.isNewline) {
+            if rawCalories.isEmpty {
+                maxCalories = max(maxCalories, currentElfCarry)
+                currentElfCarry = 0
+                continue
+            }
+
+            currentElfCarry += Int(rawCalories) ?? 0
+        }
+
+        return String(maxCalories)
     }
 
     private func solvePart2(_ inputData: String) -> String? {
-        nil
+        var elfCarryValues: [Int] = []
+
+        var currentElfCarry = 0
+        for rawCalories in inputData.split(omittingEmptySubsequences: false, whereSeparator: \.isNewline) {
+            if rawCalories.isEmpty {
+                elfCarryValues.append(currentElfCarry)
+                currentElfCarry = 0
+                continue
+            }
+
+            currentElfCarry += Int(rawCalories) ?? 0
+        }
+
+        let topThreeValuesSum = elfCarryValues.sorted().suffix(3).reduce(0, +)
+
+        return String(topThreeValuesSum)
     }
 }
